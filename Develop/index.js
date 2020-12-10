a4b936a0d89c3343afe5979b428ad215b0b8381d
Nicholas Werner <ncwerner@gmail.com>
2020-12-10 12:06:04 -0700

    added const for inquirer and moved my questions into question array

--- Develop/index.js
+++ Develop/index.js
@@ -1,30 +1,34 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const inquirer = require("inquirer")
+
 // array of questions for user
-// const questions = [
 
-// ];
-inquirer.prompt([
-    // {
-    //     type: "confirm",
-    //     message: "Would you like to make a readme file?",
-    //     name: "makeFile",
-    // },
-    {
-        type: "checkbox",
-        message: "Please choose the options you'd like in your reade file?",
-        name: "choicesReadme",
-        choices: ["## Intstallation", "## Usage", "## Credits", "## License"]
-    }
-]).then(function(responce) {
-    console.log(responce);
-    const choicesReadme = JSON.stringify(responce)
-
-    fs.writeFile("./README.md", choicesReadme, function(err) {
-        if (err) throw err;
-        console.log("Success!");
+const questions = [
+    inquirer.prompt([
+        // {
+        //     type: "confirm",
+        //     message: "Would you like to make a readme file?",
+        //     name: "makeFile",
+        // },
+        {
+            type: "checkbox",
+            message: "Please choose the options you'd like in your reade file?",
+            name: "choicesReadme",
+            choices: ["## Intstallation", "## Usage", "## Credits", "## License"]
+        }
+    ]).then(function(responce) {
+        console.log(responce);
+        const choicesReadme = JSON.stringify(responce)
+    
+        fs.writeFile("./README.md", choicesReadme, function(err) {
+            if (err) throw err;
+            console.log("Success!");
+        })
     })
-})
+    
+
+];
 // // function to write README file
 // function writeToFile(fileName, data) {
 // }
