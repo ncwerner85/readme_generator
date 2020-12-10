a5f55f19bca5f28ea9e4d7e1aa6be46e2f1a83c8
Nicholas Werner <ncwerner@gmail.com>
2020-12-10 12:54:59 -0700

    updated questions

--- Develop/index.js
+++ Develop/index.js
@@ -7,13 +7,54 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 const questions = [
     {
-        type: "checkbox",
-        message: "Please choose the options you'd like in your reade file?",
-        name: "choicesReadme",
-        choices: ["## Intstallation", "## Usage", "## Credits", "## License"]
+      type: "input",
+      name: "github",
+      message: "What is your GitHub username?"
+    },{
+      type: "input",
+      name: "email",
+      message: "What is your email address?"
+    },
+    {
+      type: "input",
+      name: "title",
+      message: "What is your project's name?"
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Please write a short description of your project"
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "What kind of license should your project have?",
+      choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "What command should be run to install dependencies?",
+      default: "npm i"
+    },
+    {
+      type: "input",
+      name: "test",
+      message: "What command should be run to run tests?",
+      default: "npm test"
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "What does the user need to know about using the repo?",
+    },
+    {
+      type: "input",
+      name: "contributing",
+      message: "What does the user need to know about contributing to the repo?",
     }
-];
-// // function to write README file
+  ];
+  // // function to write README file
 function writeToFile(fileName, data) {
     return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
